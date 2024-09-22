0543dd65d9ef176fe80f555865c61728f3d53c3f
WoodenBrick1 <brickwooden@gmail.com>
2024-09-22 12:14:53 +0200

    Added basic calculator functionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
 
+const display = document.getElementById("display-text");
+const clearButton = document.getElementById("clear");
+const equalsButton = document.getElementById("equals");
 
-
-
+const numbers = [...document.querySelectorAll("#numbers > button")];
+const operators = [...document.querySelectorAll("#operations > button")];
 const add = (a, b) => a + b;
 
 const substract = (a, b) => a - b;
@@ -33,7 +36,56 @@ const operator = (num1, num2, operator) =>
     }
 }
 
-let num1 = 2;
-let num2 = 3;
-let op = '+';
+let num1 = "";
+let num2 = "";
+let op = "";
 
+let secondNumber = false;
+
+
+const updateDisplay = (num) =>
+{
+   display.textContent += num;
+
+    if (!secondNumber)
+        num1 += num;
+    else 
+        num2 += num;
+    
+   
+}
+
+const processOperator = (operator) =>
+{
+    let displayInput = String(display.textContent);
+    if (displayInput == "" || ['+', '-', '*', '/'].includes(displayInput.slice(-1)))
+        return;
+
+    display.textContent += operator;
+    op = operator;
+    secondNumber = true;
+
+
+}
+
+const reset = () =>
+{
+    display.textContent = "";
+    num1 = "";
+    num2 = "";
+    op = "";
+    secondNumber = false;
+}
+numbers.forEach(button => button.addEventListener("click", () => updateDisplay(button.textContent)));
+
+operators.forEach(button => button.addEventListener("click", () => processOperator(button.textContent)));
+clearButton.addEventListener("click", reset)
+
+equalsButton.addEventListener("click", () =>
+{
+
+    let result = operator(parseInt(num1), parseInt(num2), op);
+    reset();
+    display.textContent = result;
+    num1 = result;
+})
